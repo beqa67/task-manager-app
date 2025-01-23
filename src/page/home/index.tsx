80b5e0a3430e304c06957e9b7ca922f79e8218ba
Beqa Lazarashvili <beka@Beqas-MacBook-Pro.local>
2025-01-23 11:50:15 +0400

    add taskManagement slice

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,12 +1,14 @@
-import {tasks} from "../../fakeData/tasks";
+
 import Card from "../../components/card";
+import {useSelector} from "react-redux";
+import {RootState} from "../../state/store";
 
 const HomePage = () => {
-
+  const tasks = useSelector((state: RootState) => state.taskManagement.tasks)
+  console.log('tasks', tasks)
   return (
      <div className="row">
-       {
-         tasks.map((task) => (
+       { tasks && tasks.map((task) => (
             <div className="col-3 mb-6">
               <Card data={task} />
             </div>
